b92901adee34d32b9243428c9e6e26eff6352891
miku <martin.czygan@gmail.com>
2010-07-22 18:00:00 +0200

    tabs to spaces; added README

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@
 from distutils.core import setup
 
 setup(
-	name='cachedurl',
-	version='0.1',
-	author='Martin Czygan',
-	author_email='martin.czygan@gmail.com',
-	url='http://myyn.org',
-	py_modules=['cachedurl'],
-	description="""
-		SHA256-based compressed URL cache infrastructure 
-		as simple as it can get.""",
-	long_description="""
-		Save bandwidth and server resources through caching. 
-		Default location is .cachedurlcache/ under user's home.""",
-	license='BSD',
-	platforms = ["any"],
-	classifiers=["Development Status :: 4 - Beta",
-		"Intended Audience :: Developers",
-		"Programming Language :: Python", 
-	],
+    name='cachedurl',
+    version='0.1',
+    author='Martin Czygan',
+    author_email='martin.czygan@gmail.com',
+    url='http://myyn.org',
+    py_modules=['cachedurl'],
+    description="""
+        SHA256-based compressed URL cache infrastructure 
+        as simple as it can get.""",
+    long_description="""
+        Save bandwidth and server resources through caching. 
+        Default location is .cachedurlcache/ under user's home.""",
+    license='BSD',
+    platforms = ["any"],
+    classifiers=["Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python", 
+    ],
 )
 
